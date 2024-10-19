f1a4df34f5e92811cd86efc13196897338626793
Florian Heuberger <fh@flogersoft.de>
2024-10-19 07:17:54 +0200

    added routes and mongo db connection

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route";
+import customerRoutes from "./routes/customer.route";
+import mongoose, { Mongoose } from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // Bodyparser
+
+// MongoDB connection
+const connectDB = async () => {
+  try {
+    console.log("HERE", process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("MongoDB connected");
+  } catch (err: any) {
+    console.error(err.message);
+    process.exit(1); // Exit server if connection failed
+  }
+};
+
+// Routes
+app.use("/auth", authRoutes);
+app.use("/customers", customerRoutes);
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Start connection to db
+connectDB();
