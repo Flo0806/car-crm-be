0beb2ca277d9fb45aff73448de4b7d82a9334042
Florian Heuberger <fh@flogersoft.de>
2024-10-19 08:05:25 +0200

    added logic to auto generate ' intNr'

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -34,11 +34,28 @@ export const createCustomer = async (
   req: Request,
   res: Response
 ): Promise<any> => {
-  const { intNr, type, contactPersons, addresses } = req.body;
+  const { type, contactPersons, addresses } = req.body;
 
   try {
+    // Find the highest intNr in the db
+    const lastCustomer = await Customer.findOne().sort({ intNr: -1 });
+
+    let newIntNr: string;
+
+    if (lastCustomer) {
+      // Extract the numeric part of intNr (e.g. "K-0002" -> 2)
+      const lastIntNrNum = parseInt(lastCustomer.intNr.split("-")[1], 10);
+
+      // Increment the number add format it back to a string (e.g. 2 -> "K-0003")
+      newIntNr = `K-${String(lastIntNrNum + 1).padStart(4, "0")}`;
+    } else {
+      // No customer? Start with 1
+      newIntNr = "K-0001";
+    }
+
+    // Generate the customer
     const newCustomer = new Customer({
-      intNr,
+      intNr: newIntNr,
       type,
       contactPersons,
       addresses,
@@ -67,15 +84,25 @@ export const updateCustomer = async (
   const { intNr, type, contactPersons, addresses } = req.body;
 
   try {
-    const updatedCustomer = await Customer.findByIdAndUpdate(
-      req.params.id,
-      { intNr, type, contactPersons, addresses },
-      { new: true }
-    );
+    // Try find the existing customer
+    const existingCustomer = await Customer.findById(req.params.id);
 
-    if (!updatedCustomer) {
+    if (!existingCustomer) {
       return res.status(404).json({ msg: "Customer not found" });
     }
+
+    // Update the data, but igrnore the `intNr`! We never should change this number!
+    const updatedCustomer = await Customer.findByIdAndUpdate(
+      req.params.id,
+      {
+        type,
+        contactPersons,
+        addresses,
+        // intNr will be the same like before
+        intNr: existingCustomer.intNr,
+      },
+      { new: true }
+    );
 
     res.json(updatedCustomer);
   } catch (err: any) {
